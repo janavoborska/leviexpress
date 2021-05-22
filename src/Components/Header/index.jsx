c9a00dee5f28b1c40ea1f79141c9289e2723575d
janavoborska <jana.dostalov@seznam.cz>
2021-05-22 10:59:43 +0200

    :dokoncen ukol 2

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1 className="site-title">
+          <a href="/">LeviExpress</a>
+        </h1>
+      </div>
+    </header>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Footer } from './Components/Footer';
+
+import { Header } from './Components/Header';
 import './style.css';
 
 const App = () => (
   <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+    <Header />
+    <Footer />
   </div>
 );
 
